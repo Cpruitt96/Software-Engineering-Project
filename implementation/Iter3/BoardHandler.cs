c9e64a411a5ceedc7b5293a6ae3cbe84f7cc4b17
lanwoodall423 <31048810+lanwoodall423@users.noreply.github.com>
2017-12-03 23:36:27 -0500

    Add files via upload

---
+++ implementation/Iter3/BoardHandler.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardHandler : MonoBehaviour {
+    private GameState gameState = GameState.PLAYING;
+
+    public int boardSize;
+    public GameObject[,,] boards;
+
+    public int currentPlayer;
+    public int[] playerShips;
+
+    public GameObject cell;
+
+    private Vector2 mousePos;
+
+	// Use this for initialization
+	void Start () {
+        currentPlayer = 0;
+        playerShips = new int[2];
+        playerShips[0] = 0;
+        playerShips[1] = 0;
+
+        boards = new GameObject[2, boardSize, boardSize];
+        //Players
+        for (int i = 0; i < 2; i++)
+        {
+            //X
+            for (int j = 0; j < boardSize; j++)
+            {
+                //Y
+                for (int k = 0; k < boardSize; k++)
+                {
+                    GameObject currentCell = Instantiate(cell);
+                    currentCell.transform.position = new Vector3(j, k + (i*30), 0);
+                    CellHandler cellHandler = currentCell.GetComponent<CellHandler>();
+                    cellHandler.xValue = i;
+                    cellHandler.yValue = j;
+                    boards[i, j, k] = currentCell;
+
+                }
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        GameController.changeGameState += updateGameState;
+    }
+
+    private void OnDisable()
+    {
+        GameController.changeGameState -= updateGameState;
+    }
+
+    void updateGameState(GameState gameState)
+    {
+        this.gameState = gameState;
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+    public void placeShip()
+    {
+        if (isPlaceable() && gameState == GameState.PLAYING)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            boards[currentPlayer, (int)Mathf.Round(mousePos.x), (int)Mathf.Round(mousePos.y) - (currentPlayer * 30)].GetComponent<CellHandler>().hasShip = true;
+            playerShips[currentPlayer] += 1;
+            GameObject.Find("TurnManager").GetComponent<TurnManager>().shipsRemaining[currentPlayer]--;
+        }
+    }
+
+    public bool isPlaceable()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if ((int)Mathf.Round(mousePos.x) >= 0 && (int)Mathf.Round(mousePos.x) <= boardSize && (int)Mathf.Round(mousePos.y) >= 0 + (currentPlayer * 30) && (int)Mathf.Round(mousePos.y) <= boardSize + (currentPlayer * 30) && !boards[currentPlayer, (int)Mathf.Round(mousePos.x), (int)Mathf.Round(mousePos.y) - (currentPlayer * 30)].GetComponent<CellHandler>().hasShip)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool attack()
+    {
+        if (isAttackable() && gameState == GameState.PLAYING)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log((3 % (currentPlayer +2)) + " " + (int)Mathf.Round(mousePos.x) + " " + ((int)Mathf.Round(mousePos.y) - (currentPlayer * 30)));
+            if(boards[(3 % (currentPlayer + 2)), (int)Mathf.Round(mousePos.x), ((int)Mathf.Round(mousePos.y) - (currentPlayer * 30))].GetComponent<CellHandler>().hasShip)
+            {
+                boards[(3 % (currentPlayer + 2)), (int)Mathf.Round(mousePos.x), ((int)Mathf.Round(mousePos.y) - (currentPlayer * 30))].GetComponentInChildren<SpriteRenderer>().color = Color.red;
+                boards[currentPlayer, (int)Mathf.Round(mousePos.x), ((int)Mathf.Round(mousePos.y) - (currentPlayer * 30))].GetComponentInChildren<SpriteRenderer>().color = Color.green;
+                GameObject.Find("TurnManager").GetComponent<TurnManager>().shipsRemaining[3 % (currentPlayer + 2)]--;
+                currentPlayer = 3 % (currentPlayer + 2);
+            } else
+            {
+                boards[currentPlayer, (int)Mathf.Round(mousePos.x), (int)Mathf.Round(mousePos.y) - (currentPlayer * 30)].GetComponentInChildren<SpriteRenderer>().color = Color.grey;
+                currentPlayer = 3 % (currentPlayer + 2);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public bool isAttackable()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if ((int)Mathf.Round(mousePos.x) >= 0 && (int)Mathf.Round(mousePos.x) <= boardSize && (int)Mathf.Round(mousePos.y) >= 0 + (currentPlayer * 30) && (int)Mathf.Round(mousePos.y) <= boardSize + (currentPlayer * 30) && boards[currentPlayer, (int)Mathf.Round(mousePos.x), (int)Mathf.Round(mousePos.y) - (currentPlayer * 30)].GetComponentInChildren<SpriteRenderer>().color != Color.grey)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ implementation/Iter3/GameController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState { MENU, MENU_TO_PLAYING, PLAYING, PLAYING_TO_MENU };
+public enum MenuState { NONE, MAIN, INVENTORY };
+
+
+public class GameController : MonoBehaviour
+{
+    private GameState _gameState;
+    private MenuState _menuState;
+    public Text gameStateDisplay;
+    private Vector3 originalPosition;
+    public GameState gameState
+    {
+        set
+        {
+            _gameState = value;
+            if (changeGameState != null)
+            {
+                changeGameState(gameState);
+            }
+        }
+        get
+        {
+            return _gameState;
+        }
+    }
+    public MenuState menuState
+    {
+        set
+        {
+            _menuState = value;
+            if (changeMenu != null)
+            {
+                changeMenu(menuState);
+            }
+        }
+        get
+        {
+            return _menuState;
+        }
+    }
+
+    public static event Action<MenuState> changeMenu;
+    public static event Action<GameState> changeGameState;
+    public static event Action reset;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        switchToMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (gameState)
+        {
+            case GameState.MENU:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    gameState = GameState.MENU_TO_PLAYING;
+                }
+                break;
+            case GameState.MENU_TO_PLAYING:
+                switchToPlaying();
+                break;
+            case GameState.PLAYING:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    gameState = GameState.PLAYING_TO_MENU;
+                }
+                if (Input.GetKeyDown(KeyCode.I) && menuState != MenuState.INVENTORY)
+                {
+                    switchToInventory();
+                }
+                else if (Input.GetKeyDown(KeyCode.I))
+                {
+                    switchToPlaying();
+                }
+                break;
+            case GameState.PLAYING_TO_MENU:
+                switchToMenu();
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    public void switchToPlaying()
+    {
+        gameState = GameState.PLAYING;
+        menuState = MenuState.NONE;
+        gameStateDisplay.text = "Playing";
+    }
+
+    public void switchToMenu()
+    {
+        gameState = GameState.MENU;
+        menuState = MenuState.MAIN;
+        gameStateDisplay.text = "Menu";
+    }
+
+    public void switchToInventory()
+    {
+        menuState = MenuState.INVENTORY;
+        gameStateDisplay.text = "Inventory";
+    }
+
+    public void switchToMenu(MenuState newMenu)
+    {
+        menuState = newMenu;
+    }
+
+    public void restart()
+    {
+        if (reset != null)
+        {
+            reset();
+        }
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
+
